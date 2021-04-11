79b68a45697785975a1da39e750d4e8745ad3c8c
Pritam <pritamdebnath297@gmail.com>
2021-04-12 03:16:39 +0530

    feat : created a form field for todo

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import TextField from '@material-ui/core/TextField';
+import {useState} from "react";
+import { Button } from '@material-ui/core';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [todoInput, setTodoInput] = useState('');
+
+  //to upload it to database
+  // function addTodo(e){
+  //   
+  //   e.preventDefault();
+  //   console.log(`Trying to add todo`);
+  // }
+
+  const addTodo=(e)=>{
+    e.preventDefault();    //prevent from reloading
+    console.log(`You are tryinng to add todo`);
+  }
+  //   
+  
+
+  return ( 
+  <div className = "App"
+    style = {
+      {
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center"
+      }
+    } >
+    <h1 > Your To - Do list </h1> 
+    <form>
+    <TextField id = "standard-basic"
+    label = "Write your Task" 
+    value={todoInput}
+    onChange={(e)=>setTodoInput(e.target.value)}
+    />
+    <Button type='submit' variant="contained" onClick={addTodo}>Default</Button>
+    </form>
     </div>
   );
 }
---
+++ to-do-list/src/firebase_config.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyBF2fFswWiA4w7H_iOnUUE7PhSutrCmRhI",
+    authDomain: "to-do-list-e840d.firebaseapp.com",
+    projectId: "to-do-list-e840d",
+    storageBucket: "to-do-list-e840d.appspot.com",
+    messagingSenderId: "19475025945",
+    appId: "1:19475025945:web:83474538f8c646fb071a88"
+  };
+
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+export {db};
